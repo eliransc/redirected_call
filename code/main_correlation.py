90c18e9936cdd6d263e93f7a273dc130623153b0
eliran <scherzter@gmail.com>
2021-06-29 10:40:04 +0300

    going over different values of h0

--- code/main_correlation.py
+++ code/main_correlation.py
@@ -32,85 +32,89 @@ def main(args):
     ub_vals = np.linspace(ub_low, ub_high, 1).astype(int)
 
     h0 = 3
-    t1_path = '../pkl/' + str(lam0) + '_' + str(lam1) + '_' + str(args.mu0) + '_' + str(args.mu1) + '_' + str(
-        h0) + 't1_dict.pkl'
 
-    sum_res = pd.DataFrame([],columns=('lam0','lam1','mu0','mu1','avg_station_1','inter_depart_type_1'))
-    if not os.path.exists(sum_res_full_path):
-        pkl.dump(sum_res, open(sum_res_full_path, 'wb'))
+    h0_list = [0.5,1,1.5,2,2.5,3]
+    for h0 in h0_list:
 
+        t1_path = '../pkl/' + str(lam0) + '_' + str(lam1) + '_' + str(args.mu0) + '_' + str(args.mu1) + '_' + str(
+            h0) + 't1_dict.pkl'
 
-    for ind_ub_v, ub_v in enumerate(ub_vals):
-
-        t_prob_path = '../pkl/' + str(ub_v) + '_' + str(lam0) + '_' + str(lam1) + '_' + str(
-            args.mu0) + '_' + str(args.mu1)+ '_' + str(h0) + 't_prob.pkl'
-
-        mean_num_rates_ub_v_path = os.path.join(pkl_path, str(ub_v) + '_' + str(lam0) + '_' + str(lam1) + '_' + str(
-            args.mu0) + '_' + str(args.mu1)+ '_' + str(h0) + 'mean_nam_rate_ub_v.pkl')
-
-        df_name_before = 'df_' + str(ub_v)+'_'+str(lam0)+'_'+str(lam1) +'_'+str(args.mu0)+'_'+str(args.mu1) + '_' + str(h0)+ '_before_probs.pkl'
-        df_name_before = os.path.join(pkl_path, df_name_before)
-
-        df_name_after = 'df_' + str(ub_v) +'_'+str(lam0)+'_'+str(lam1)+'_'+str(args.mu0)+'_'+str(args.mu1) + '_' + str(h0) + '_after_probs.pkl'
-        df_name_after = os.path.join(pkl_path, df_name_after)
-
-        print('stage 1: compute general structure')
-        if not os.path.exists(df_name_before):
-            give_number_cases(ub_v, df_name_before)
-        print('stage 2: compute marginal probs')
-        if not os.path.exists(df_name_after):
-            compute_df(args.mu0, args.mu1, lam0, lam1, df_name_before, df_name_after, ub_v, mean_num_rates_ub_v_path, args, True)
-
-        # df_result = pkl.load(open(df_name_after, 'rb'))
-        if not os.path.exists(t_prob_path):
-            t_shape = compute_bayesian_probs(lam0, lam1, args.mu0, args.mu1,  args.eps, df_name_after, t_prob_path, h0)
-        if not os.path.exists(t1_path):
-            create_t_1_probs(df_name_after,  lam0, lam1, args.mu0, args.mu1, t_prob_path, h0, t1_path)
+        sum_res = pd.DataFrame([],columns=('lam0','lam1','mu0','mu1','avg_station_1','inter_depart_type_1'))
+        if not os.path.exists(sum_res_full_path):
+            pkl.dump(sum_res, open(sum_res_full_path, 'wb'))
 
 
+        for ind_ub_v, ub_v in enumerate(ub_vals):
 
-        df_name_after_non_eq = 'df_' + str(ub_v) + '_' + str(lam0) + '_' + str(lam1) + '_' + str(args.mu0) + '_' + str(
-            args.mu1)+ '_' + str(h0) + '_after_probs_non_eq.pkl'
-        df_name_after_non_eq = os.path.join(pkl_path, df_name_after_non_eq)
+            t_prob_path = '../pkl/' + str(ub_v) + '_' + str(lam0) + '_' + str(lam1) + '_' + str(
+                args.mu0) + '_' + str(args.mu1)+ '_' + str(h0) + 't_prob.pkl'
 
-        h_arr = np.linspace(0.1, 10, 25)
+            mean_num_rates_ub_v_path = os.path.join(pkl_path, str(ub_v) + '_' + str(lam0) + '_' + str(lam1) + '_' + str(
+                args.mu0) + '_' + str(args.mu1)+ '_' + str(h0) + 'mean_nam_rate_ub_v.pkl')
 
-        cond_list = []
-        uncond_list = []
+            df_name_before = 'df_' + str(ub_v)+'_'+str(lam0)+'_'+str(lam1) +'_'+str(args.mu0)+'_'+str(args.mu1) + '_' + str(h0)+ '_before_probs.pkl'
+            df_name_before = os.path.join(pkl_path, df_name_before)
 
-        df_eq = pkl.load(open(df_name_after, 'rb'))
-        t1_prob = pkl.load(open(t1_path,'rb'))
+            df_name_after = 'df_' + str(ub_v) +'_'+str(lam0)+'_'+str(lam1)+'_'+str(args.mu0)+'_'+str(args.mu1) + '_' + str(h0) + '_after_probs.pkl'
+            df_name_after = os.path.join(pkl_path, df_name_after)
 
+            print('stage 1: compute general structure')
+            if not os.path.exists(df_name_before):
+                give_number_cases(ub_v, df_name_before)
+            print('stage 2: compute marginal probs')
+            if not os.path.exists(df_name_after):
+                compute_df(args.mu0, args.mu1, lam0, lam1, df_name_before, df_name_after, ub_v, mean_num_rates_ub_v_path, args, True)
 
-        for h in tqdm(h_arr):
-
-            tot_curr_denst = 0
-
-            for key in t1_prob.keys():
-
-                compute_df(args.mu0, args.mu1, lam0, lam1, df_name_before, df_name_after_non_eq, ub_v, mean_num_rates_ub_v_path,
-                           args, False, np.array(t1_prob[key]))
-
-                curr_dens = get_curr_dens(df_name_after_non_eq, args.mu0, args.mu1, lam0, lam1, h)
-                curr_prob = df_eq.loc[int(key), 'baysian_prob']
-                tot_curr_denst += curr_dens*curr_prob
-
-            cond_list.append(tot_curr_denst)
-
-            uncod_dens = get_curr_dens(df_name_after, args.mu0, args.mu1, lam0, lam1, h)
-            uncond_list.append(uncod_dens)
+            # df_result = pkl.load(open(df_name_after, 'rb'))
+            if not os.path.exists(t_prob_path):
+                t_shape = compute_bayesian_probs(lam0, lam1, args.mu0, args.mu1,  args.eps, df_name_after, t_prob_path, h0)
+            if not os.path.exists(t1_path):
+                create_t_1_probs(df_name_after,  lam0, lam1, args.mu0, args.mu1, t_prob_path, h0, t1_path)
 
 
-        plt.figure()
-        plt.plot(h_arr, cond_list, label = 'conditioned',  linestyle='dashed', color = 'blue', alpha = 0.6,linewidth=5, )
-        plt.plot(h_arr, uncond_list, label = 'prior', alpha = 0.6, color = 'green', linewidth=5)
-        plt.legend()
-        plt.savefig('cond_dist' +str(lam0)+'_'+str(lam1)+'_'+str(args.mu0)+'_'+str(args.mu1)+'_'+str(h0)+'.png')
-        plt.show()
 
-        print('the end')
+            df_name_after_non_eq = 'df_' + str(ub_v) + '_' + str(lam0) + '_' + str(lam1) + '_' + str(args.mu0) + '_' + str(
+                args.mu1)+ '_' + str(h0) + '_after_probs_non_eq.pkl'
+            df_name_after_non_eq = os.path.join(pkl_path, df_name_after_non_eq)
 
-        pkl.dump((h_arr, cond_list, uncond_list), open('../pkl/h_arr_cond_dist_uncond_dist' +str(lam0)+'_'+str(lam1)+'_'+str(args.mu0)+'_'+str(args.mu1)+'_'+str(h0)+'.pkl', 'rb'))
+            h_arr = np.linspace(0.1, 10, 25)
+
+            cond_list = []
+            uncond_list = []
+
+            df_eq = pkl.load(open(df_name_after, 'rb'))
+            t1_prob = pkl.load(open(t1_path,'rb'))
+
+
+            for h in tqdm(h_arr):
+
+                tot_curr_denst = 0
+
+                for key in t1_prob.keys():
+
+                    compute_df(args.mu0, args.mu1, lam0, lam1, df_name_before, df_name_after_non_eq, ub_v, mean_num_rates_ub_v_path,
+                               args, False, np.array(t1_prob[key]))
+
+                    curr_dens = get_curr_dens(df_name_after_non_eq, args.mu0, args.mu1, lam0, lam1, h)
+                    curr_prob = df_eq.loc[int(key), 'baysian_prob']
+                    tot_curr_denst += curr_dens*curr_prob
+
+                cond_list.append(tot_curr_denst)
+
+                uncod_dens = get_curr_dens(df_name_after, args.mu0, args.mu1, lam0, lam1, h)
+                uncond_list.append(uncod_dens)
+
+
+            plt.figure()
+            plt.plot(h_arr, cond_list, label = 'conditioned',  linestyle='dashed', color = 'blue', alpha = 0.6,linewidth=3, )
+            plt.plot(h_arr, uncond_list, label = 'prior', alpha = 0.6, color = 'green', linewidth=3)
+            plt.legend()
+            plt.savefig('cond_dist' +str(lam0)+'_'+str(lam1)+'_'+str(args.mu0)+'_'+str(args.mu1)+'_'+str(h0)+'.png')
+            plt.show()
+
+            print('the end')
+
+            pkl.dump((h_arr, cond_list, uncond_list), open('../pkl/h_arr_cond_dist_uncond_dist' +str(lam0)+'_'+str(lam1)+'_'+str(args.mu0)+'_'+str(args.mu1)+'_'+str(h0)+'.pkl', 'rb'))
 
 
 

9d25d65b3e6d45985706bce4cc2844209c980cc1
eliran <scherzter@gmail.com>
2021-12-18 23:33:42 -0500

    new sttings

--- code/main_accuracy.py
+++ code/main_accuracy.py
@@ -20,8 +20,8 @@ def main(args):
     sum_results_name = 'sum_result_new1.pkl'
     pkl_path = r'../pkl'
     sum_res_full_path = os.path.join(pkl_path,sum_results_name)
-    ub_high = 14
-    ub_low = 14
+    ub_high = 17
+    ub_low = 17
     ub_vals = np.linspace(ub_low, ub_high, 1).astype(int)
     lam0s = np.linspace(0.5, 0.5, 1)
     total_arr = np.zeros([ub_high-ub_low+1, lam0s.shape[0]])

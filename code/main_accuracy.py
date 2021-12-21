b78c22848acaaf712e389070a5947505a255f2db
eliran <scherzter@gmail.com>
2021-12-20 21:02:50 -0500

    lower eps

--- code/main_accuracy.py
+++ code/main_accuracy.py
@@ -17,7 +17,7 @@ import time
 
 def main(args):
 
-    sum_results_name = 'sum_result_many4.pkl'
+    sum_results_name = 'sum_result_many5.pkl'
     pkl_path = r'../pkl'
     sum_res_full_path = os.path.join(pkl_path,sum_results_name)
     ub_high = 3
@@ -31,10 +31,10 @@ def main(args):
     if not os.path.exists(sum_res_full_path):
         pkl.dump(sum_res, open(sum_res_full_path, 'wb'))
 
-    # df = pd.read_excel(r'C:\Users\elira\workspace\Research\versions_settings.xlsx', sheet_name='python')
-    df = pkl.load(
-        open('/gpfs/fs0/scratch/d/dkrass/eliransc/redirected_git/redirected_call/code/diff_settings.pkl', 'rb'))
-    for ind in range(df.shape[0]):
+    df = pd.read_excel(r'C:\Users\elira\workspace\Research\versions_settings.xlsx', sheet_name='python')
+    # df = pkl.load(
+    #     open('/gpfs/fs0/scratch/d/dkrass/eliransc/redirected_git/redirected_call/code/diff_settings.pkl', 'rb'))
+    for ind in tqdm(range(df.shape[0])):
 
         lam0 = df.loc[ind,'lambda00']
         lam1 = df.loc[ind,'lambda01']

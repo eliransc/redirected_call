17841c8d56468ad871857ba8997114000373bf49
eliran <scherzter@gmail.com>
2021-12-21 22:01:54 -0500

    long runs when lam00 =3

--- code/main_accuracy.py
+++ code/main_accuracy.py
@@ -35,6 +35,8 @@ def main(args):
     df = pkl.load(
         open('/gpfs/fs0/scratch/d/dkrass/eliransc/redirected_git/redirected_call/code/diff_settings.pkl', 'rb'))
 
+
+
     df = df.loc[df['lambda00']==3,:]
     df = df.reset_index()
     for ind in tqdm(range(df.shape[0])):

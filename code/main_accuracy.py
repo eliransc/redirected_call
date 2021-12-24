8c31af2f2ba84de08fd6f612136a9ee708b995b8
eliran <scherzter@gmail.com>
2021-12-24 14:22:25 -0500

    new settings for accuracy

--- code/main_accuracy.py
+++ code/main_accuracy.py
@@ -33,7 +33,7 @@ def main(args):
 
     # df = pd.read_excel(r'C:\Users\elira\workspace\Research\versions_settings.xlsx', sheet_name='python1')
     df = pkl.load(
-        open('/gpfs/fs0/scratch/d/dkrass/eliransc/redirected_git/redirected_call/code/diff_settings_new.pkl', 'rb'))
+        open('/gpfs/fs0/scratch/d/dkrass/eliransc/redirected_git/redirected_call/code/diff_settings.pkl', 'rb'))
 
 
 

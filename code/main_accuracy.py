87e05d5e502f5dd2058dcb6a5fa223e28b5e10f8
eliran <scherzter@gmail.com>
2021-12-31 10:01:11 -0500

    new settings for accuracy

--- code/main_accuracy.py
+++ code/main_accuracy.py
@@ -31,12 +31,12 @@ def main(args):
     if not os.path.exists(sum_res_full_path):
         pkl.dump(sum_res, open(sum_res_full_path, 'wb'))
 
-    # df = pd.read_excel(r'C:\Users\elira\workspace\Research\versions_settings.xlsx', sheet_name='python')
+    # df = pd.read_excel(r'C:\Users\elira\workspace\Research\diff_settings_util0.xlsx', sheet_name='python')
     df = pkl.load(
         open('/gpfs/fs0/scratch/d/dkrass/eliransc/redirected_git/redirected_call/code/diff_settings.pkl', 'rb'))
 
 
-    for ind in [15]:
+    for ind in [29]:
 
         lam0 = df.loc[ind,'lambda00']
         lam1 = df.loc[ind,'lambda01']
@@ -53,7 +53,7 @@ def main(args):
         elif lam0 == 1:
             ub_v = 12
         else:
-            ub_v = 27
+            ub_v = 26
 
 
 

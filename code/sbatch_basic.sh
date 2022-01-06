aeece30d35cff0f27b24ec185bbb912e5ac85c2a
eliran <scherzter@gmail.com>
2022-01-06 10:14:08 -0500

    adding sbach file

--- code/sbatch_basic.sh
+++ code/sbatch_basic.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
-#SBATCH --mem 10000
 source /home/eliransc/.virtualenvs/deep_queue/bin/activate
 python /scratch/d/dkrass/eliransc/inter_departure/redirected_call/code/sim_two_stations_two_classes.py
 

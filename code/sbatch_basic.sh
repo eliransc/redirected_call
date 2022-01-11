5102be7e6a059a0e5b64864ad28b8c244a52a08e
eliransc <scherzter@gmail.com>
2022-01-10 21:08:32 -0500

    trial

--- code/sbatch_basic.sh
+++ code/sbatch_basic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 0-15:58
 #SBATCH -A def-dkrass
-source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate #/home/d/dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/inter_departure/redirected_call/code/sim_two_stations_two_classes.py #/scratch/d/dkrass/eliransc/inter_departure/redirected_call/code/sim_two_stations_two_classes.py
+source /home/d/dkrass/eliransc/queues/bin/activate
+python /scratch/d/dkrass/eliransc/inter_departure/redirected_call/code/sim_two_stations_two_classes.py
 
---
+++ code/sbatch_cedar_sim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -t 0-15:58
+#SBATCH -A def-dkrass
+source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
+python /home/eliransc/projects/def-dkrass/eliransc/inter_departure/redirected_call/code/sim_two_stations_two_classes.py
+

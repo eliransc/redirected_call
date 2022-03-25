b76c170e70cf7191920553fd5110bcb3216ee1d5
eliransc <scherzter@gmail.com>
2022-03-24 22:12:14 -0400

    kl

--- code/main_correlation.py
+++ code/main_correlation.py
@@ -24,11 +24,11 @@ def main(args):
     lam1 = args.lam1
 
 
-    sum_results_name = 'sum_result20.pkl'
+    sum_results_name = 'sum_result21.pkl'
     pkl_path = r'../pkl'
     sum_res_full_path = os.path.join(pkl_path,sum_results_name)
-    ub_high = 4
-    ub_low = 4
+    ub_high = 7
+    ub_low = 7
     ub_vals = np.linspace(ub_low, ub_high, 1).astype(int)
 
 
@@ -191,8 +191,8 @@ def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--correlation', type=bool, help='computing_correlation', default=True)
     parser.add_argument('--ub_v', type=int, help='v_max', default=11)
-    parser.add_argument('--mu0', type=float, help='mu0', default=0.75)
-    parser.add_argument('--mu1', type=float, help='mu1', default=1.8)
+    parser.add_argument('--mu0', type=float, help='mu0', default=1)
+    parser.add_argument('--mu1', type=float, help='mu1', default=1.5)
     parser.add_argument('--lam0', type=float, help='mu0', default=0.25)
     parser.add_argument('--lam1', type=float, help='mu0', default=0.75)
     parser.add_argument('--lam_ext', type=float, help='external arrival to sub queue', default=0.5)

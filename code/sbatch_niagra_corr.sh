edf58a4c41f3e3776d6adde35017e51e4dadc6c2
eliransc <scherzter@gmail.com>
2022-03-26 22:12:13 -0400

    kl

--- code/sbatch_niagra_corr.sh
+++ code/sbatch_niagra_corr.sh
@@ -2,5 +2,5 @@
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
 source /home/d/dkrass/eliransc/queues/bin/activate
-python /scratch/d/dkrass/eliransc/inter_departure/redirected_call/code/main_correlation.py
+python /scratch/d/dkrass/eliransc/new_redircted/redirected_call/code/main_correlation.py
 

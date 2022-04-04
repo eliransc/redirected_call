e3501d97462a6cdeb64d998fb66f0d728d65c429
eliransc <scherzter@gmail.com>
2022-04-04 12:49:51 -0400

    kl

--- code/sbatch_narval_corr.sh
+++ code/sbatch_narval_corr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
+#SBATCH --mem-per-cpu=10000M
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/redirected_call/code/main_correlation.py

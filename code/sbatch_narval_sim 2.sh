f8b538b82927c9eb9a78c760dc7e4019854c2e99
eliransc <scherzter@gmail.com>
2022-04-06 15:05:35 -0400

    kl

--- code/sbatch_narval_sim 2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -t 0-23:58
-#SBATCH -A def-dkrass
-#SBATCH --mem-per-cpu=10000M
-source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/redirected_call/code/sim_rho_0.py

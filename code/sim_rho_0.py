2d7128cbce657034116725ab579a26a951dfeda6
eliransc <scherzter@gmail.com>
2022-04-08 16:17:26 -0400

    kl

--- code/sim_rho_0.py
+++ code/sim_rho_0.py
@@ -13,15 +13,15 @@ from utils import *
 import random
 
 
-# case_ind = 9# random.randint(0, 23)
-init_path = '/home/eliransc/projects/def-dkrass/eliransc/redirected_call/code/init_list_30.pkl'
-if not os.path.exists(init_path):
-    pkl.dump(np.arange(30),open(init_path, 'wb'))
-
-initial_list = pkl.load(open(init_path, 'rb'))
-case_ind = np.random.choice(initial_list)
-initial_list = np.delete(initial_list, np.where(initial_list == case_ind))
-pkl.dump(initial_list, open(init_path, 'wb'))
+case_ind = random.randint(0, 30)
+# init_path = '/home/eliransc/projects/def-dkrass/eliransc/redirected_call/code/init_list_35.pkl'
+# if not os.path.exists(init_path):
+#     pkl.dump(np.arange(30),open(init_path, 'wb'))
+#
+# initial_list = pkl.load(open(init_path, 'rb'))
+# case_ind = np.random.choice(initial_list)
+# initial_list = np.delete(initial_list, np.where(initial_list == case_ind))
+# pkl.dump(initial_list, open(init_path, 'wb'))
 
 def main(args):
 

439447ab3789e67934e7979f2aed037e3f0e4210
eliransc <scherzter@gmail.com>
2022-04-13 11:36:47 -0400

    kl

--- code/sim_rho_0.py
+++ code/sim_rho_0.py
@@ -149,34 +149,34 @@ def main(args):
 
 
             if not os.path.exists(args.df_summ):
-                df = pd.DataFrame([])
+                df_ = pd.DataFrame([])
             else:
-                df = pkl.load(open(args.df_summ, 'rb'))
-            ind = df.shape[0]
+                df_ = pkl.load(open(args.df_summ, 'rb'))
+            ind = df_.shape[0]
 
-            df.loc[ind, 'lam00'] = lam00
-            df.loc[ind, 'lam01'] = lam01
-            df.loc[ind, 'lam10'] = lam10
-            df.loc[ind, 'lam11'] = lam11
+            df_.loc[ind, 'lam00'] = lam00
+            df_.loc[ind, 'lam01'] = lam01
+            df_.loc[ind, 'lam10'] = lam10
+            df_.loc[ind, 'lam11'] = lam11
 
-            df.loc[ind, 'mu00'] = mu00
-            df.loc[ind, 'mu01'] = mu01
-            df.loc[ind, 'mu10'] =  0 #mu10
-            df.loc[ind, 'mu11'] = mu11
+            df_.loc[ind, 'mu00'] = mu00
+            df_.loc[ind, 'mu01'] = mu01
+            df_.loc[ind, 'mu10'] =  0 #mu10
+            df_.loc[ind, 'mu11'] = mu11
 
-            df.loc[ind, 'avg_cust_0'] = avg_waiting[0] * (lam00+lam01)
-            df.loc[ind, 'avg_cust_1'] = df_summary_result.loc[0, 'avg_sys_1']
-            df.loc[ind, 'avg_wait_0'] = avg_waiting[0]
-            df.loc[ind, 'avg_wait_1'] = avg_waiting[1]
+            df_.loc[ind, 'avg_cust_0'] = avg_waiting[0] * (lam00+lam01)
+            df_.loc[ind, 'avg_cust_1'] = df_summary_result.loc[0, 'avg_sys_1']
+            df_.loc[ind, 'avg_wait_0'] = avg_waiting[0]
+            df_.loc[ind, 'avg_wait_1'] = avg_waiting[1]
             # df.loc[ind,'var_0'] = df_inter_departure_station_0['inter_departure_time'].var()
 
-            df.loc[ind, 'ind'] = case_ind
+            df_.loc[ind, 'ind'] = case_ind
             corr_time = pkl.load(open(corr_path, 'rb'))
-            df.loc[ind, 'inter_rho'] = corr_time[-1]
+            df_.loc[ind, 'inter_rho'] = corr_time[-1]
 
-            pkl.dump(df, open(args.df_summ,'wb'))
+            pkl.dump(df_, open(args.df_summ,'wb'))
 
-            print(df)
+            print(df_)
 
 
 def avg_sys_station_0(r ,mu,ind):
@@ -316,14 +316,14 @@ def parse_arguments(argv):
     parser.add_argument('--r', type=np.array, help='external arrivals', default=np.array([]))
     parser.add_argument('--number_of_classes', type=int, help='number of classes', default=2)
     parser.add_argument('--mu', type=np.array, help='service rates', default=np.array([]))
-    parser.add_argument('--end_time', type=float, help='The end of the simulation', default=100)
+    parser.add_argument('--end_time', type=float, help='The end of the simulation', default=10000000)
     parser.add_argument('--size', type=int, help='the number of stations in the system', default=2)
     parser.add_argument('--p_correct', type=float, help='the prob of external matched customer', default=0.5)
     parser.add_argument('--ser_matched_rate', type=float, help='service rate of matched customers', default=1.2)
     parser.add_argument('--ser_mis_matched_rate', type=float, help='service rate of mismatched customers', default=10.)
     parser.add_argument('--num_iterations', type=float, help='service rate of mismatched customers', default=1)
     parser.add_argument('--case_num', type=int, help='case number in my settings', default=random.randint(0, 100000))
-    parser.add_argument('--df_summ', type=str, help='case number in my settings', default='../pkl/df_sum_res_sim_111.pkl')
+    parser.add_argument('--df_summ', type=str, help='case number in my settings', default='../pkl/df_sum_res_sim_29.pkl')
     parser.add_argument('--is_corr', type=bool, help='should we keep track on inter departure', default=True)
 
     args = parser.parse_args(argv)

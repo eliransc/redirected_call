868d2e5c1c15a27ab12a95803149d8eebaac0fd2
eliransc <scherzter@gmail.com>
2022-04-28 10:48:13 -0400

    kl

--- code/sim_rho_0.py
+++ code/sim_rho_0.py
@@ -34,7 +34,7 @@ def main(args):
     # initial_list = np.delete(initial_list, np.where(initial_list == case_ind))
     # pkl.dump(initial_list, open(init_path, 'wb'))
 
-    for case_ind in range(10, 15):
+    for case_ind in range(15, 20):
 
 
         print(case_ind)

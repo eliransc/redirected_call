a7a0e237adc1debe6ee9bfacab91b966b9b41163
eliransc <scherzter@gmail.com>
2022-05-02 15:16:21 -0400

    kl

--- code/sbatch_niagrasim2.sh
+++ code/sbatch_niagrasim2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
-#SBATCH --mem-per-cpu=10000M
 source /home/d/dkrass/eliransc/queues/bin/activate
 python /scratch/d/dkrass/eliransc/new_redircted/redirected_call/code/sim_rho_0.py
